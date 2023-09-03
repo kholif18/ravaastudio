ab6254ea5f36223b0175d2988fedf0620d0e26d6
ravaastudio <ravaastudio@gmail.com>
2023-09-03 08:29:07 +0700

    img

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Ashion Template">
-    <meta name="keywords" content="Ashion, unica, creative, html">
+    <meta name="description" content="Ravaa Studio Template">
+    <meta name="keywords" content="Ravaa Studio, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,20 +1,31 @@
+<!-- Offcanvas Menu Begin -->
+<div class="offcanvas-menu-overlay"></div>
+<div class="offcanvas-menu-wrapper">
+    <div class="offcanvas__close">+</div>
+    <div class="offcanvas__logo">
+        <a href="{{ url('/') }}"><img src="img/logo.svg" alt=""></a>
+    </div>
+    <div id="mobile-menu-wrap"></div>
+</div>
+<!-- Offcanvas Menu End -->
+
 <!-- Header Section Begin -->
 <header class="header">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xl-3 col-lg-2">
+            <div class="col-xl-4 col-lg-2">
                 <div class="header__logo">
                     <a href="{{ url('/') }}"><img src="{{ asset('img/logo.svg') }}" alt="Logo"></a>
                 </div>
             </div>
-            <div class="col-xl-9 col-lg-10">
+            <div class="col-xl-8 col-lg-10">
                 <nav class="header__menu">
                     <ul>
                         <li class="active"><a href="{{ url('/') }}">Home</a></li>
-                        <li><a href="#">Women’s</a></li>
-                        <li><a href="#">Men’s</a></li>
-                        <li><a href="#">Shop</a></li>
-                        <li><a href="#">Pages</a>
+                        <li><a href="#">Banner</a></li>
+                        <li><a href="#">Sablon</a></li>
+                        <li><a href="#">Content Design</a></li>
+                        <li><a href="#">Undangan</a>
                             <ul class="dropdown">
                                 <li><a href="./product-details.html">Product Details</a></li>
                                 <li><a href="./shop-cart.html">Shop Cart</a></li>
@@ -22,7 +33,6 @@
                                 <li><a href="./blog-details.html">Blog Details</a></li>
                             </ul>
                         </li>
-                        <li><a href="./blog.html">Blog</a></li>
                         <li><a href="./contact.html">Contact</a></li>
                     </ul>
                 </nav>
